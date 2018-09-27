9a45c722e5a2817f611d9154a5d07320f5f3de9d
John Beshir <john@beshir.org>
2018-09-27 19:55:43 +0100

    Implement basic data splitting and loading.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from keras.models import Model
 from keras.layers import Dense, Input, LSTM
 
@@ -5,8 +6,28 @@ from keras.layers import Dense, Input, LSTM
 n_a = 5
 
 X = Input((None, 1))
-x = LSTM(n_a)(X)
-Y = Dense(1, activation='softmax')(x)
+X1 = LSTM(n_a)(X)
+Y = Dense(1, activation='softmax')(X1)
 
 model = Model(inputs=[X], outputs=[Y])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+def loaddata(summaryfile):
+    summarydata = pd.read_csv(summaryfile, sep=',', header=None)
+    allresponses = pd.read_csv('responsedata.csv', sep=',', header=None)
+
+    x = []
+    y = []
+    for i in range(summarydata.shape[0]):
+        responses = allresponses[(allresponses[0] == summarydata.iloc[i, 0]) & allresponses[2].notnull()]
+        x.append(responses[2].values.tolist())
+        y.append(1 if summarydata.iloc[i, 5] == 1 else 0)
+
+    return x, y
+
+
+# Load training data
+Xtrain, Ytrain = loaddata('summarydata-train.csv')
+for i in range(100):
+    print(Xtrain[i], Ytrain[i])
--- splitdata.py
+++ splitdata.py
@@ -1,8 +1,21 @@
+import numpy as np
 import pandas as pd
 
-# Load in all response data.
-AllData = pd.read_csv('responsedata.csv', sep=',', header=None)
+# Load in all response summary data.
+AllSummaryData = pd.read_csv('summarydata.csv', sep=',', header=None)
 
-UniquePredictions = AllData[0].unique()
+AllShuffledSummaryData = AllSummaryData[AllSummaryData[5] != 0].sample(frac=1)
 
-print(AllData[0].unique())
+n_test = np.floor(AllShuffledSummaryData.shape[0] * 0.2).astype(np.int64)
+n_cv = n_test
+n_train = AllShuffledSummaryData.shape[0] - n_cv - n_test
+
+TrainSummaryData = AllShuffledSummaryData.iloc[0:n_train, :]
+CVSummaryData = AllShuffledSummaryData.iloc[n_train:n_train+n_cv, :]
+TestSummaryData = AllShuffledSummaryData.iloc[n_train+n_cv:, :]
+
+TrainSummaryData.to_csv('summarydata-train.csv', index=False, header=False)
+CVSummaryData.to_csv('summarydata-cv.csv', index=False, header=False)
+TestSummaryData.to_csv('summarydata-test.csv', index=False, header=False)
+
+print(n_train, n_cv, n_test)
